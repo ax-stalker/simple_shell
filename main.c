bca7c3f7591e802de54870d616c8cd882b723c84
ax-stalker <64146780+ax-stalker@users.noreply.github.com>
2023-07-29 06:57:33 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,30 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - Entry point of the shell.
+ * @ac: Argument count.
+ * @av: Argument vector.
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success, 1 on error.
  */
 int main(int ac, char **av)
 {
 	info_t info[] = { INFO_INIT };
 	int fd = 2;
 
+	// Redirect file descriptor 2 (stderr) to fd (useful for testing).
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
 		: "=r" (fd)
 		: "r" (fd));
 
+	// If a script is provided as an argument, open it for reading.
 	if (ac == 2)
 	{
 		fd = open(av[1], O_RDONLY);
 		if (fd == -1)
 		{
+			// Handle file open errors and exit with appropriate codes.
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
@@ -37,8 +40,13 @@ int main(int ac, char **av)
 		}
 		info->readfd = fd;
 	}
+
+	// Populate the environment list and read history from file.
 	populate_env_list(info);
 	read_history(info);
+
+	// Start the shell.
 	hsh(info, av);
+
 	return (EXIT_SUCCESS);
 }
